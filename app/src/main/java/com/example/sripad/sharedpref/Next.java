b2937f8baa345789f052b059c53d4ecbe5307703
Sripad K <sripadkmandrawadkar@gmail.com>
2018-09-07 12:31:03 +0530

    small changes

--- app/src/main/java/com/example/sripad/sharedpref/Next.java
+++ app/src/main/java/com/example/sripad/sharedpref/Next.java
@@ -21,6 +21,11 @@ public class Next extends AppCompatActivity {
 */
 
 
+/*
+
+        Button bt=findViewById(R.id.bt2);
+
+*/
             Button bt=findViewById(R.id.bt2);
 
         bt.setOnClickListener(new View.OnClickListener() {
